2ad2d78801b416d88cfee0334021357c66c88a5a
ludndev <ludndev@gmail.com>
2024-02-22 11:44:22 +0100

    add get_accounts function

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+
+
+def get_accounts(start_dir="."):
+    for root, dirs, files in os.walk(start_dir):
+        if "accounts.csv" in files:
+            return os.path.join(root, "accounts.csv")
+    return None
